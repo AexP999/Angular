4f234107b6305a30c0d29ed798e63d07302efce8
AlexP999 <mikol999@gmail.com>
2021-06-03 21:23:24 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,16 @@
 import { RouterModule } from '@angular/router';
-import { PostComponent } from './components/post/post.component';
-import { PostsComponent } from './components/posts/posts.component';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component';
-import { UsersComponent } from './components/users/users.component';
+
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-	declarations: [ AppComponent, UsersComponent, UserComponent, PostsComponent, PostComponent ],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		HttpClientModule,
-		RouterModule.forRoot([
-			{ path: 'users', component: UsersComponent },
-			{ path: 'home', component: HomeComponent }
-		])
-	],
+	declarations: [ AppComponent ],
+	imports: [ BrowserModule, AppRoutingModule, HttpClientModule ],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
