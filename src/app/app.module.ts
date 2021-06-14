4323b398259e340cd65ef12ac6c1c1d965a49bb3
AlexP999 <mikol999@gmail.com>
2021-06-14 11:20:50 +0300

    3. Create a Feature Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
 	imports: [ CommonModule, BrowserModule, FormsModule ],
-	declarations: [ AppComponent, HeroesComponent ],
+	declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
 	providers: [],
 	bootstrap: [ AppComponent ]
 })
