ccae9d3a319c0b4659427ca04d03f1538ccf50af
AlexP999 <mikol999@gmail.com>
2021-06-14 10:12:19 +0300

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './components/app.component';
-import { PostsComponent } from './modules/posts-total/posts-comonents/posts/posts.component';
-import { PostComponent } from './modules/posts-total/posts-comonents/post/post.component';
-
-let routes: Routes = [
-	{
-		path: 'users',
-		loadChildren: () => import('./modules/users-total/users.module').then((m) => m.UsersModule)
-	},
-	// {
-	// 	path: 'posts',
-	// 	component: PostsComponent,
-	// 	children: [ { path: ':id', component: PostDetailsComponent } ]
-	// }
-	{
-		path: 'posts',
-		loadChildren: () => import('./modules/posts-total/posts.module').then((m) => m.PostsModule)
-	}
-];
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-	declarations: [ AppComponent ],
-
-	imports: [ BrowserModule, AppRoutingModule, HttpClientModule, RouterModule.forRoot(routes) ],
-
+	imports: [ CommonModule, BrowserModule, FormsModule ],
+	declarations: [ AppComponent, HeroesComponent ],
 	providers: [],
-
 	bootstrap: [ AppComponent ]
 })
 export class AppModule {}
--- src/app/modules/models/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-	id: number;
-	username: string;
-	email: string;
-	address: object;
-	// city: string;
-	name: string;
-}
